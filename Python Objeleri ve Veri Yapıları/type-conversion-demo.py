db7eee543863d33300b3d839af8726d40fcc87f4
H-P-U <unal.pinar.htc@gmail.com>
2020-03-19 18:55:57 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/type-conversion-demo.py
@@ -0,0 +1,15 @@
+'''
+Daire alanı: pi r^2
+Daire çevresi: 2pir
+
+Yarıçapı verilen dairenin alanını ve çevresini hesaplayınız. pi=3,14
+
+'''
+
+r= float(input ("yari çap: "))
+pi= 3.14
+daireAlan= pi*(r**2)
+daireCevre= 2*pi*r
+"""print("alan: ", daireAlan)
+print("çevre: ",daireCevre)"""
+print('alan: '+ str(daireAlan)+ ' çevre: '+ str(daireCevre))
