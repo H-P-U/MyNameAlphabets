0df52113611db25a91250eb9510da9ef269c11ce
H-P-U <unal.pinar.htc@gmail.com>
2020-03-19 14:46:44 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/variables.py
@@ -0,0 +1,34 @@
+maasHatice = 5000
+maasPinar = 4000
+Vergi= 0.27
+
+print(maasHatice - (maasHatice * Vergi))
+
+print(maasPinar - (maasPinar * Vergi))
+
+ # Değişken yazım kuralları
+ # değişken rakamla başlayamaz
+ #büyük küçük harf duyarlıdır
+
+sayi1 = 10
+print(sayi1)
+
+sayi1 = 20
+print(sayi1)
+
+sayi1 += 30
+print(sayi1)
+
+# x, y, ad, isStudent = (1, 2.3, "Hatice" , True)
+x= 1                   #int
+y=2.3                  #float
+ad = "Hatice"          #string
+isStudent = True       #bool
+
+a = "10"
+b = "40"
+print(a+b) #1020
+
+ilkad= "Hatice"
+ikinciad= " Pinar"
+print(ilkad+  ikinciad)  #Hatice Pinar
