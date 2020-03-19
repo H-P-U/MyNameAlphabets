78bf507b534242a8171dd80e1bfa59b8354f07cd
H-P-U <unal.pinar.htc@gmail.com>
2020-03-19 19:16:04 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/string.py
@@ -0,0 +1,16 @@
+name= 'Hatice Pinar'
+surname= 'Ünal'
+age= 25
+
+#print('My name is '+name+' '+ surname+ ' and \nI am '+ str(age)+ ' years old.')
+
+greeting= 'My name is '+name+' '+ surname+ ' and I am '+ str(age)+ ' years old.'
+length= len(greeting)
+#print(greeting)
+#print(greeting[0])
+#print(greeting[length-1])
+#print(greeting[-1])
+#print(greeting[2:6])
+#print(greeting[2:])
+#print(greeting[:9])
+print(greeting[2:40:3])
