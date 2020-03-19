fa0e7cfb17ccb61b7db49dfbf17ffbaa91b5ea3d
H-P-U <unal.pinar.htc@gmail.com>
2020-03-19 19:35:49 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/format.py
@@ -0,0 +1,12 @@
+name= "Hatice Pinar"
+surname= "Ünal"
+age= 25
+
+#print('My name is {} {}' .format(name, surname))
+#print('My name is {1} {0}' .format(name, surname))
+#print("My name is {} {} and I'm  {} years old.".format(name,surname, age))
+
+"""result= 200/700
+print("The result is {r:1.2} ".format(r=result))"""
+
+print(f"My name is {name} {surname} and I'm {age} years old.")
