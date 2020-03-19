f63a09a24511edfd99429734d380e498f256587a
H-P-U <unal.pinar.htc@gmail.com>
2020-03-19 18:42:05 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/type-conversion.py
@@ -0,0 +1,41 @@
+"""x=input('1.sayi:')
+y=input('2.sayi:')
+
+print(type(x))
+print(type(y))
+toplam = int(x)+int(y)
+print(toplam)"""
+
+x=5                     #int
+y=7.3                   #float
+ad= 'Hatice'            #string
+isOnline = True         #bool
+"""print(type(x))
+print(type(y))
+print(type(ad))
+print(type(isOnline))"""
+
+#int to float
+
+"""x= float(x)
+print(x)
+print(type(x))"""
+
+#float to int
+
+"""y=int(y)
+print(y)
+print(type(y))"""
+
+"""result= str(x) + str(y)
+print(result)
+print(type(result))"""
+
+"""isOnline= str(isOnline)
+print(isOnline)
+print(type(isOnline))"""
+
+#bool to int
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
---
+++ Python Objeleri ve Veri Yapıları/variables_demo.py
@@ -0,0 +1,32 @@
+'''
+1-Müşterinin aşağıdaki bilgileri için değişken oluşturun
+  Müşteri Adı
+  Müşteri Soyadı
+  Müşteri Adı+Soyadı
+  Müşteri Cinsiyet
+  Müşteri TC Kimlik No
+  Müşteri Doğum Yılı
+  Müşteri Adresi
+  Müşteri Yaşı
+'''
+customerName = "Hatice Pinar"
+customerSurname = " Ünal"
+print(customerName+ customerSurname)
+customerGender= False #Kadın
+customerTC= "11111111111"
+customerBth= 1995
+customerAdress= "İstanbul"
+customerAge= 2020-customerBth
+print(customerAge)
+
+"""
+Aşağıdaki siparişlerin toplam bilgisini hesapla.
+Sipariş1=>110 TL
+Sipariş2=>1100.5 TL
+Sipariş3=>356.95 TL
+"""
+siparis1= 110
+siparis2=1100.5
+siparis3=356.95
+ToplamSiparis = siparis1 + siparis2 + siparis3
+print("Toplam:", ToplamSiparis)
