82314dae2c3d824599309209c4f2723ad90161e2
H-P-U <unal.pinar.htc@gmail.com>
2020-03-20 14:32:59 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/lists.py
@@ -0,0 +1,23 @@
+#message= "Hello there.I am HPÜ"
+#print(message[])
+
+#my_list=[1,2,3]
+#my_list=['bir',2, 3.5, True]
+#print(my_list)
+
+'''list1= ['one', 'two', 'three']
+list2=['four','five','six']
+numbers= list1 + list2
+print(numbers)
+#len kaç eleman olduğunu verir.
+print(len(numbers))
+print(message[0])
+print(numbers[2])'''
+
+userA= ['Pınar', 25]
+userB=['Pelin', 23]
+users= [userA+ userB]
+"""print(userA)
+print(userB)
+print(users)"""
+print(users[0][1])
