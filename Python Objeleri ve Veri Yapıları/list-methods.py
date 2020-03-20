b20e6877507e33e091611179c58f11bc5b0a938f
H-P-U <unal.pinar.htc@gmail.com>
2020-03-20 16:22:47 +0300

    updates

---
+++ Python Objeleri ve Veri Yapıları/list-methods.py
@@ -0,0 +1,63 @@
+numbers=[1, 10, 5, 16, 4, 9, 10]
+letters=['a','g','s','b','y','a', 's']
+
+#numbers listesi içerisindeki max sayı ve min sayı değeri bulmak için
+'''val= min(numbers)
+val= max(numbers)
+#letters listesi içinde en son harf(max) ve ilk harfi(min) bulmak için
+val= max(letters)
+val= min(letters)
+print(val)'''
+#listenin 3-6 arasındaki elemanları yazdırmak için
+'''val= numbers[3:6]
+print(val)
+#listenin ilk 3 elamanı yazdırmak için
+val=number[:3]
+print(val)
+#4. indeksten sona kadar
+val=number[4:]
+print(val)
+#5. indeksi değiştir
+numbers[5]=40
+print(numbers)
+#listeye eleman ekle
+numbers.append(25)
+print(numbers)
+#listede istediğin sıraya eleman ekle
+numbers.insert(4,30)
+numbers.insert(-1, 55)
+print(numbers)
+#listeden eleman silmek için
+numbers.pop()
+print(numbers)
+numbers.pop(0)
+print(numbers)
+#silmek istediğinkarakteri yazarak listeden çıkarmak için
+numbers.remove(5)
+print(numbers)
+#listeyi sıralı yazdırmak için
+numbers.sort()
+letters.sort()
+print(numbers)
+print(letters)
+#listeyi tersten  yazdırmak için
+numbers.reverse()
+letters.reverse()
+print(numbers)
+print(letters)
+#listeyi tersten sıralı yazdırmak için
+numbers.sort()
+numbers.reverse()
+letters.sort()
+letters.reverse()
+print(numbers)
+print(letters)
+#eleman sayısını bulmak için
+print(len(numbers))
+print(len(letters))
+#listede yer alan elemanın kaç tane olduğunu bulmak için
+print(numbers.count(10))
+print(letters.count('a'))'''
+#bütün elemanları silmek için
+numbers.clear()
+print(numbers)
