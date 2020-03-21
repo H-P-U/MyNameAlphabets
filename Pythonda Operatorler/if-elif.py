db46ac142040a404ec2eb6b519a8f2e889c7f1f0
H-P-U <unal.pinar.htc@gmail.com>
2020-03-21 22:38:33 +0300

    updates

---
+++ Pythonda Operatorler/if-elif.py
@@ -0,0 +1,18 @@
+'''x=int(input('X: '))
+y=int(input('Y: '))
+
+if x>y:
+    print('x büyüktür y')
+elif x==y:
+    print('x eşittir y')
+else:
+    print('x küçüktür y')'''
+
+say= int(input('sayı giriniz: '))
+
+if say>0:
+    print('Pozitif Sayı')
+elif say==0:
+    print('Nötr')
+else:
+    print('Negatif Sayı')
