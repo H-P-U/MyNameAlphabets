cddb7a62e822a6e6c94c0ddf8401e1940330b060
H-P-U <unal.pinar.htc@gmail.com>
2020-03-21 12:01:28 +0300

    updates

---
+++ Pythonda Operatorler/assignment-demo.py
@@ -0,0 +1,26 @@
+x, y, z= 2, 5, 10
+
+numbers= 1, 5, 7, 10, 6
+
+#1-Kullanıcıdan aldığınız iki sayının çarpımı ile x,y,z toplamının farkı nedir?
+'''sayi1=input('Sayi gir:')
+sayi2=input('Sayi gir:')
+result= (int(sayi1) * int(sayi2))-(x+y+z)
+print(result)'''
+#2- y nin x e kalansız bölümünü bulunuz.
+'''result= y//x
+print(result)'''
+#3-(x,y,z) toplamının mod3 ü nedir?
+'''result= (x+y+z)%3
+print(result)'''
+#4-y'nin x inci kuvvetini hesaplayınız.
+'''result= y**x
+print(result)'''
+#5- X, *y, z= numbers işlemine göre z'nin 3. kuvveti kaçtır?
+'''x, *y, z= numbers
+result= z**3
+print(result)'''
+#6- x, *y, z= numbers işlemine göre y'nin değerleri toplammı kaçtır?
+'''x, *y, z= numbers
+result= (y[0]+y[1]+y[2])
+print(result)'''
