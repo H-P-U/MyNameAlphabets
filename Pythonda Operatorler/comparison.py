ca36d75bb246eef4733326bdb3b569c54a525b70
H-P-U <unal.pinar.htc@gmail.com>
2020-03-21 12:31:52 +0300

    updates

---
+++ Pythonda Operatorler/comparison.py
@@ -0,0 +1,19 @@
+#username, password => Database
+# 'HPU', ' 123456'
+
+a,b,c,d=5, 5, 10, 4
+password= '1234'
+username= 'HPU'
+result= (a == b)        #True
+result= (a != b)        #False
+result= (a == c)        #False
+result= (a != c)        #True
+result= (a > c)         #False
+result= (a < c)         #True
+result= (a >= b)        #True
+result= (c <= b)        #False
+result= (True == 1)
+result= ( False == 0)
+result= ('HtcPnr' == username)
+result= ('HPU' == username)
+print(result)
