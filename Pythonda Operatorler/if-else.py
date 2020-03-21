ca2fdd73275c0a39f9219788fca58136f71457e5
H-P-U <unal.pinar.htc@gmail.com>
2020-03-21 22:27:20 +0300

    updates

---
+++ Pythonda Operatorler/if-else.py
@@ -0,0 +1,18 @@
+username='HPU'
+password= '123456'
+
+'''isLoggedin= (username=='HPU') and (password=='123456')
+
+if isLoggedin:
+    print('Hoşgeldiniz:)')
+else:
+    print('Parola ya da kullanıcı bilgisi hatalı')'''
+
+
+if (username=='HPU'):
+    if (password=='123456'):
+        print('Hoşgeldiniz:)')
+    else:
+        print('Parola Hatalı')
+else:
+        print('Kullanıcı bilgisi hatalı')
