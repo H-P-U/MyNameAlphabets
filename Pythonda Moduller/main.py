1d7c2c6628a5b2ebe0f8d7ec8aee6673233b01c1
H-P-U <unal.pinar.htc@gmail.com>
2020-03-29 22:47:32 +0300

    updates

---
+++ Pythonda Moduller/main.py
@@ -0,0 +1,15 @@
+import mod
+
+#result=help(mod)
+#result=help(mod.func)
+result=mod.number
+result=mod.numbers
+result=mod.person["name"]
+result=mod.person["city"]
+result=mod.func(9)
+
+p=mod.Person()
+p.speak()
+
+print(result)
+
