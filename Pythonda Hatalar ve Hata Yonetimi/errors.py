733e870234d6dfe36f741d0041168c53cb3813cd
H-P-U <unal.pinar.htc@gmail.com>
2020-03-30 12:28:52 +0300

    updates

---
+++ Pythonda Hatalar ve Hata Yonetimi/errors.py
@@ -0,0 +1,10 @@
+#errors => Hatalar
+
+#error handling=> Eror Yönetimi
+
+#print(a) =>NameError
+#int('1a2')=> ValueError
+#print(10/0) => ZeroDivisionError
+#print('Merhab'a) => SyntaxError
+
+'''python.org, docs, lib references,built-in-exceptions sayfasında alınabilecek hataların hepsi yer alıyor'''
